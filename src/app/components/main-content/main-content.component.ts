04c1dc09e91d3cb6449750f7bfbacde177de4051
IanSantosM <ian.santos@macro.com.br>
2026-02-17 12:26:34 -0300

    feat: add main content component with template and initial tests, update import path in default layout

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { WelcomeCardComponent } from "../../../components/welcome-card/welcome-card.component";
-import { TodoListComponent } from "../../../components/todo-list/todo-list.component";
+import { WelcomeCardComponent } from "../welcome-card/welcome-card.component";
+import { TodoListComponent } from "../todo-list/todo-list.component";
 
 @Component({
   selector: 'app-main-content',
