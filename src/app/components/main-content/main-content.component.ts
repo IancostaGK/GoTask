6e96d5eaf7765ccf7b359b87c2df665a9ead1de1
IanSantosM <ian.santos@macro.com.br>
2026-02-17 13:55:12 -0300

    feat: implement todo list container component and update main content layout

--- src/app/components/main-content/main-content.component.ts
+++ src/app/components/main-content/main-content.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { WelcomeCardComponent } from "../welcome-card/welcome-card.component";
-import { TodoListComponent } from "../todo-list/todo-list.component";
+import { TodoListContainerComponent } from "../todo-list-container/todo-list-container.component";
 
 @Component({
   selector: 'app-main-content',
-  imports: [WelcomeCardComponent, TodoListComponent],
+  imports: [WelcomeCardComponent, TodoListContainerComponent],
   templateUrl: './main-content.component.html',
 })
 export class MainContentComponent {
---
+++ src/app/components/todo-list-container/todo-list-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list-container',
+  imports: [],
+  templateUrl: './todo-list-container.component.html',
+  styleUrl: './todo-list-container.component.css'
+})
+export class TodoListContainerComponent {
+
+}
