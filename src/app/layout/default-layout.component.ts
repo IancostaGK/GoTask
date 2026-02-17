06769da101b65516f2a9d9604008396e07ee487f
IanSantosM <ian.santos@macro.com.br>
2026-02-17 12:24:37 -0300

    feat: add header component with layout and initial tests

--- src/app/layout/default-layout.component.ts
+++ src/app/layout/default-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from '../components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 
 @Component({
